dcd65a66260d2a06e9f41a8ec8399b2697533d7d
Boxcat0 <vv5454@naver.com>
2023-03-30 08:38:11 +0900

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Navbar from "./components/Navbar";
+import LoginButton from "./components/LoginButton";
+import Title from "./components/Title";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [showLogin, setShowLogin] = useState(false);
+    return (
+        <div>
+            <Navbar onClick={() => setShowLogin(!showLogin)} />
+            <div className={`login-menu ${showLogin ? 'show-login-menu' : ''}`}>
+                <LoginButton />
+            </div>
+            <div className = "title-Center">
+            <Title />
+            </div>
+            <SearchBar />
+        </div>
+    );
 }
 
 export default App;
