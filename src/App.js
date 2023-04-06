86f61bbff3cc52a35e5b4de2ddab2767682d775c
Boxcat0 <vv5454@naver.com>
2023-04-06 10:07:27 +0900

    1st

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Title from './components/Title';
+import SearchBar from './components/SearchBar';
+import LoginButton from './components/LoginButton';
+import LoginPage from './components/LoginPage';
 import './App.css';
-import Navbar from "./components/Navbar";
-import LoginButton from "./components/LoginButton";
-import Title from "./components/Title";
-import SearchBar from "./components/SearchBar";
-import {BrowserRouter} from "react-router-dom";
 
 function App() {
     const [showLogin, setShowLogin] = useState(false);
     return (
-       <BrowserRouter>
-           <div>
-               <Navbar onClick={() => setShowLogin(!showLogin)} />
-               <div className={`login-menu ${showLogin ? 'show-login-menu' : ''}`}>
-                   <LoginButton />
-               </div>
-               <div className = "title-Center">
-                   <Title />
-               </div>
-               <SearchBar />
-           </div>
-       </BrowserRouter>
+        <BrowserRouter>
+            <div>
+                <Navbar onClick={() => setShowLogin(!showLogin)} />
+                <div className={`login-menu ${showLogin ? 'show-login-menu' : ''}`}>
+                    <LoginButton />
+                </div>
+                <div className="title-Center">
+                    <Title />
+                </div>
+                <Routes>
+                    <Route exact path="/" element={<SearchBar />} />
+                    <Route exac path="/login" element={<LoginPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 function LoginPage() {
     return (
-        <div className="login-page">
-            <h1>어디가GYM</h1>
+        <div className="login">
             <form>
                 <div>
-                    <label htmlFor="username">아이디:</label>
+                    <label htmlFor="username">아이디:
                     <input type="text" id="username" name="username" />
+                    </label>
                 </div>
                 <div>
-                    <label htmlFor="password">비밀번호:</label>
+                    <label htmlFor="password">비밀번호:
                     <input type="password" id="password" name="password" />
+                    </label>
                 </div>
                 <button type="submit">로그인</button>
             </form>
