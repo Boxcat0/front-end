61c785e3b465735979252bcd3c2041702fb6725a
Boxcat0 <vv5454@naver.com>
2023-04-10 12:46:21 +0900

    로그인 페이지 간단하게 구현 loginPage 카카오 지도 연결 : Map 지도 중심 : 동아대학교 Map Button클릭시 지도 연결 확인-> 검색시 검색위치 기반으로 변경 요청 4/10 ->지도에 위치 기반 마커 생성
    요청사항 : 회원가입, 회원 정보를 위한 페이지 제작

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
     return (
         <div className="login">
             <form>
                 <div>
-                    <label htmlFor="username">아이디:
+                    <label htmlFor="username">아이디
                     <input type="text" id="username" name="username" />
                     </label>
                 </div>
                 <div>
-                    <label htmlFor="password">비밀번호:
+                    <label htmlFor="password">비밀번호
                     <input type="password" id="password" name="password" />
                     </label>
                 </div>
                 <button type="submit">로그인</button>
+                <Link to="/make_ID">
+                    <button>회원가입</button>
+                </Link>
             </form>
         </div>
     );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,9 +7,15 @@ function Map(){
         const container = document.getElementById("map"); // 지도를 표시할 HTML 엘리먼트
         const options = {
             center: new window.kakao.maps.LatLng(35.1167, 128.9685), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
+            level: 3 // 지도의 확대 레벨(작을 수록 확대를 더 한다)
         };
         const map = new window.kakao.maps.Map(container, options); // 지도 객체 생성
+        // 마커 생성
+        const markerPosition = new window.kakao.maps.LatLng(35.1167, 128.9685);
+        const marker = new window.kakao.maps.Marker({
+            position: markerPosition
+        });
+        marker.setMap(map); // 마커 지도에 표시
     }, []);
 
     return (
