8e02e278a06b5079f28deb2e281ae0eadac15d2a
Boxcat0 <vv5454@naver.com>
2023-04-19 16:01:10 +0900

    4월 13일 네비게이션 바 축소 + 왼쪽 정렬+검색 바에 value삽입 유튜브는 피지컬갤러리 기준 3개(수정 가능+ 하드코딩 방법 못찾음)
    searchBar 왼쪽 정렬
    아이콘 만들기
    회원가입페이지(점주 용, 일반인용)
    로그인 다시 만들기
    회원정보
    중고거래 게시판
    운동 추천 영상
    이벤트 배너
    리뷰, 개인 페이지

---
+++ src/components/myInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./menuButton";
+import Youtube from "./YoutubeSlider";
+
+function myInfo(){
+    return(
+       <div>
+           <Youtube />
+       </div>
+    )
+}
+
+export default myInfo;
