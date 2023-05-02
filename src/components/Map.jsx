0f0f050a11d8b5d8a684bdd8143bd9bab0b119a2
Boxcat0 <vv5454@naver.com>
2023-05-02 18:06:27 +0900

    4월 28일 검색창에서 주소를 입력하면 지도로 표시.(정확한 도로명 주소만 해당됨)
    searchBar 왼쪽 정렬
    아이콘 만들기
    회원정보
    중고거래 게시판
    운동 추천 영상
    이벤트 배너
    리뷰, 개인 페이지

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,38 +1,48 @@
-import React, { useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./css/map.css";
-import "./menuButton";
 
 function Map() {
+    const [data, setData] = useState({});
     let latitude = parseFloat(sessionStorage.getItem("latitude"));
     let longitude = parseFloat(sessionStorage.getItem("longitude"));
-    console.log(latitude);
-    console.log(longitude);
-    if(latitude == null || longitude == null)//디폴트값 설정
-    {
+    if (latitude == null || longitude == null) {
         latitude = 35.1167;
         longitude = 128.9685;
     }
-    useEffect(() => {
-        // 카카오 지도 API 스크립트가 로딩된 이후에 실행됩니다.
-        // 여기에서 지도를 생성하고 조작하는 코드를 작성합니다.
-        const container = document.getElementById("map"); // 지도를 표시할 HTML 엘리먼트
-        const options = {
-            center: new window.kakao.maps.LatLng(latitude, longitude), // 지도의 중심좌표
-            level: 3, // 지도의 확대 레벨(작을 수록 확대를 더 한다)
-        };
-        const map = new window.kakao.maps.Map(container, options); // 지도 객체 생성
-        // 마커 생성
-        const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
-        const marker = new window.kakao.maps.Marker({
-            position: markerPosition,
-        });
-        marker.setMap(map); // 마커 지도에 표시
-    }, [latitude, longitude]);
 
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("/gymdata.json");
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData().then();
+    }, []);
+
+    useEffect(() => {
+        if (data) {
+            const container = document.getElementById("map");
+            const options = {
+                center: new window.kakao.maps.LatLng(latitude, longitude),
+                level: 3,
+            };
+            const map = new window.kakao.maps.Map(container, options);
+            const markerPosition = new window.kakao.maps.LatLng(latitude, longitude);
+            const marketGym = new window.kakao.maps.LatLng(data.Lat, data.Lng);
+            const marker = new window.kakao.maps.Marker({
+                position: markerPosition,
+            });
+            const marker2 = new window.kakao.maps.Marker({
+                position: marketGym,
+            });
+            marker.setMap(map);
+            marker2.setMap(map);
+        }
+    }, [latitude, longitude,data]);
 
     return (
         <div className="kakaoMap">
-            <div id="map" style={{ width: "1000px", height: "700px" }}>
+            <div id="map" style={{ width: "850px", height: "550px" }}>
                 {/* 지도를 표시할 영역입니다. */}
             </div>
         </div>
