d8a493156741ad8c7be3725c98bc0561556fc6de
Boxcat0 <vv5454@naver.com>
2023-05-03 17:31:13 +0900

    4월 28일 검색창에서 주소를 입력하면 지도로 표시.(정확한 도로명 주소만 해당됨)
    searchBar 왼쪽 정렬
    아이콘 만들기
    회원정보
    중고거래 게시판
    운동 추천 영상
    이벤트 배너
    리뷰, 개인 페이지

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar"
 import Youtube from "./YoutubeSlider"
 import './css/youtube_button.css'
+import Banner from "./banner"
 
 function main(){
     return(
@@ -9,6 +10,9 @@ function main(){
             <SearchBar />
             <div className="youtube_menu">
                 <Youtube/>
+            </div>
+            <div>
+                <Banner/>
             </div>
         </div>
     )
